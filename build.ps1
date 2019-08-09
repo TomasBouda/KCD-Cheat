af3e78d882bf8ec6024acd632451c89d8e15dd3a
Tomáš Bouda <email@tomasbouda.cz>
2019-08-09 16:08:52 +0200

    windows build and release scripts

---
+++ build.ps1
@@ -0,0 +1,4 @@
+$Env:KCD_CHEAT_BUILD="true"
+$Env:KCD_CHEAT_HOME="$PSScriptRoot"
+
+lua "$PSScriptRoot\Source\Scripts\Startup\main.lua"
---
+++ release.ps1
@@ -0,0 +1,16 @@
+$VERSION = '1.31'
+$targetDir = "C:\Program Files (x86)\Steam\steamapps\common\KingdomComeDeliverance\mods\Cheat\"
+$srcDir = "$PSScriptRoot\Source"
+
+(Get-Content "$PSScriptRoot\mod.manifesttemplate") | Foreach-Object {
+	$_ -replace '{created}',  [datetime]::Now.ToString('ddd MMM dd HH:mm:ss yyyy', [Globalization.CultureInfo]::CreateSpecificCulture('en-GB')) `
+		-replace '{version}', $VERSION
+} | Out-File "$PSScriptRoot\mod.manifest"
+
+Remove-Item "$targetDir\*" -Recurse
+
+New-Item "$targetDir\Data" -ItemType Directory -Force | Out-Null
+Compress-Archive (Get-ChildItem $srcDir | Select-Object -ExpandProperty FullName) -DestinationPath "$targetDir\Data\data.zip" -Force
+Rename-Item "$targetDir\Data\data.zip" -NewName "$targetDir\Data\data.pak" -Force
+
+Move-Item "$PSScriptRoot\mod.manifest" -Destination $targetDir
